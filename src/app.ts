edfa60d500e2ef7c63d3e92772484fca19e8eec3
dnurmaxmatov <dilxushuzb2001@gmail.com>
2023-07-27 00:53:08 -0700

    configure routes

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,32 @@
-import express, {Express} from 'express'
-import { userRouter } from './users/users';
-import {Server} from 'http'
-import { LoggerService } from './logger/logger.service';
+import express, { Express } from "express";
+import { Server } from "http";
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
 
 export class App {
-    app: Express
-    port: number
-    server: Server
-    logger: LoggerService
+  app: Express;
+  port: number;
+  server: Server;
+  logger: LoggerService;
+  userController: UserController
 
-    constructor(logger: LoggerService){
-        this.app=express()
-        this.port=8000;
-        this.logger=logger
-    }
+  constructor(
+    logger: LoggerService,
+    userController: UserController
+    ) {
+    this.app = express();
+    this.port = 8000;
+    this.logger = logger;
+    this.userController=userController
+  }
 
-    useRoutes(){
-        this.app.use('./users', userRouter)
-    }
+  useRoutes() {
+    this.app.use("/users", this.userController.router);
+  }
 
-    public async init(){
-        this.useRoutes();
-        this.server=this.app.listen(this.port)
-        this.logger.log(`Server is running ${this.port}`)
-    }
+  public async init() {
+    this.useRoutes();
+    this.server = this.app.listen(this.port);
+    this.logger.log(`Server is running ${this.port}`);
+  }
 }
